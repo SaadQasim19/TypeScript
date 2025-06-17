55e2093befecfaf2ff7b6840ae5bc3757bc32aa3
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-06-17 19:20:13 +0500

    Worked On All Loops types

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -48,3 +48,72 @@ let person: {
   name: "Saad",
   age: 20,
 };
+
+
+//^ Loops
+//& JavaScript:
+for (let i = 0; i < number.length; i++) {
+  console.log(number[i]);
+}
+
+//& TypeScript:
+for (let i: number = 0; i < number.length; i++) {
+  console.log(number[i]);
+}
+
+//* While Loop
+//& JavaScript:
+let count = 0;
+while (count < 3) {
+    console.log(count);
+    count++;
+}
+
+//& TypeScript:
+let counts: number = 0;
+while (count < 3) {
+    console.log(count);
+    count++;
+}
+
+//* Do-While Loop
+
+//& JavaScript:
+let z = 0;
+do {
+    console.log(z);
+    z++;
+} while (z < 3);
+
+//& TypeScript:
+let s: number = 0;
+do {
+    console.log(z);
+    z++;
+} while (z < 3);
+
+//* For-Of Loop
+//& JavaScript:
+let fruit = ["apple", "banana"];
+for (let fruita of fruit) {
+    console.log(fruita);
+}
+
+//& TypeScript:
+let fruits: string[] = ["apple", "banana"];
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+//* For-In Loop
+//& JavaScript:
+// let personss = { name: "Ali", age: 30 };
+// for (let obj in personss) {
+//     console.log(obj + ": " + personss[obj]);
+// }
+
+//& TypeScript:
+let personData: { name: string, age: number } = { name: "Ali", age: 30 };
+for (let key in personData) {
+    console.log(`${key}: ${personData[key as keyof typeof personData]}`);
+}
